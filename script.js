c205a568e1592b2eac941fbd5ee6696394338444
Skipper <60583875+Skipper2197@users.noreply.github.com>
2025-12-13 16:05:27 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// function toggleMenu() {
+//     const navLinks = document.querySelector('.nav-links');
+//     if (navLinks) {
+//         navLinks.classList.toggle('active');
+//     }
+// }
+
+function toggleMenu() {
+  document.querySelector('.nav-links').classList.toggle('active');
+}
