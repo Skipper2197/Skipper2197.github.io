562991c4f6e4f7e0cb8534b26564a20606e89264
Skipper <60583875+Skipper2197@users.noreply.github.com>
2025-12-14 16:10:24 -0500

    test

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// function toggleMenu() {
-//     const navLinks = document.querySelector('.nav-links');
-//     if (navLinks) {
-//         navLinks.classList.toggle('active');
-//     }
-// }
+// We wanted to make the website responsive on mobile, so we created a
+// menu icon that displays a menu on click (aka on tap). This function 
+// is what displays the menu options. We also used media queries in our
+// css file to hide the menu icon when on desktop, and just display the
+// normal menu options instead. 
 
 function toggleMenu() {
   document.querySelector('.nav-links').classList.toggle('active');
